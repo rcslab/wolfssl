927d76791926221be246a5ab746b02fd805e0f9c
Aimilios Tsalapatis <atsalapa@uwaterloo.ca>
2022-08-29 19:03:01 -0400

    completely removed pre-commit hook

--- wolfcrypt/user-crypto/autogen.sh
+++ wolfcrypt/user-crypto/autogen.sh
@@ -8,7 +8,6 @@ if test -d .git; then
   if ! test -d .git/hooks; then
     mkdir .git/hooks
   fi
-  ln -s -f ../../pre-commit.sh .git/hooks/pre-commit
   ln -s -f ../../pre-push.sh .git/hooks/pre-push
 fi
 
